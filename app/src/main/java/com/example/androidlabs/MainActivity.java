cd5c27d4af67b6f49cd6a9b5e71033aff713d514
trangam <trangthuy.tt782@gmail.com>
2020-01-22 14:41:17 -0500

    add implementation

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -1,17 +1,21 @@
 package com.example.androidlabs;
 
 import androidx.appcompat.app.AppCompatActivity;
-
+import androidx.support.design.widget.Snackbar;
 import android.os.Bundle;
 import android.widget.Button;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.Toast;
+
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_linear);
+        setContentView(R.layout.activity_main_grid);
+        setContentView(R.layout.activity_main_relative);
 
         final Button button = (Button) findViewById(R.id.button);
         button.setOnClickListener(new View.OnClickListener() {
@@ -20,6 +24,16 @@ public class MainActivity extends AppCompatActivity {
                         "Here is more information",
                         Toast.LENGTH_LONG);
                 toast.show();
+            }
+        });
+        final CheckBox checkBox=(CheckBox) findViewById(R.id.checkBox);
+        checkBox.setOnCheckedChangeListener((buttonView, isChecked)-> {
+            if (checkBox.isChecked()){
+                Snackbar.make(checkBox, getResources().getString(R.string.snackbar_message_true), Snackbar.LENGTH_LONG)
+                        .setAction(getResources().getString(R.string.snackbar_setAction_message),
+                                click -> buttonView.setChecked(!isChecked) ).show();
+            }else{
+                Snackbar.make(switch1, getResources().getString(R.string.snackbar_message_false), Snackbar.LENGTH_LONG).show();
             }
         });
     }
