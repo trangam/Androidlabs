8c9a8b10b21a5545462f50d0da92c49bdb7eb913
trangam <trangthuy.tt782@gmail.com>
2020-01-23 14:33:21 -0500

    change in main

--- app/src/main/java/com/example/androidlabs/MainActivity.java
+++ app/src/main/java/com/example/androidlabs/MainActivity.java
@@ -1,12 +1,17 @@
 package com.example.androidlabs;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.support.design.widget.Snackbar;
 import android.os.Bundle;
 import android.widget.Button;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.CompoundButton;
+import android.widget.Switch;
 import android.widget.Toast;
+
+import com.google.android.material.snackbar.Snackbar;
+
+
 
 public class MainActivity extends AppCompatActivity {
 
@@ -14,10 +19,10 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_linear);
-        setContentView(R.layout.activity_main_grid);
-        setContentView(R.layout.activity_main_relative);
 
-        final Button button = (Button) findViewById(R.id.button);
+
+        Button button= findViewById(R.id.button);
+
         button.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
                 Toast toast = Toast.makeText(getApplicationContext(),
@@ -26,15 +31,37 @@ public class MainActivity extends AppCompatActivity {
                 toast.show();
             }
         });
-        final CheckBox checkBox=(CheckBox) findViewById(R.id.checkBox);
-        checkBox.setOnCheckedChangeListener((buttonView, isChecked)-> {
-            if (checkBox.isChecked()){
-                Snackbar.make(checkBox, getResources().getString(R.string.snackbar_message_true), Snackbar.LENGTH_LONG)
-                        .setAction(getResources().getString(R.string.snackbar_setAction_message),
+         CheckBox checkBox=findViewById(R.id.checkBox) ;
+        checkBox.setOnCheckedChangeListener((buttonView, isChecked )-> {
+                       if(checkBox.isChecked()){
+                           Snackbar.make(checkBox, "Checkbox is on", Snackbar.LENGTH_LONG)
+                                   .setAction("Undo",
+                                           click -> buttonView.setChecked(!isChecked) ).show();
+                       }
+                       else{
+                           Snackbar.make(checkBox, "Checkbox is off",
+                                   Snackbar.LENGTH_LONG).setAction("Undo",
+                                   click -> buttonView.setChecked(!isChecked) ).show();
+                       }
+
+                    });
+
+
+        Switch switch1 = findViewById(R.id.switch1);
+        switch1.setOnCheckedChangeListener((buttonView,isChecked)->  {
+            if (switch1.isChecked()){
+                Snackbar.make(switch1, "Switch is on", Snackbar.LENGTH_LONG)
+                        .setAction("Undo",
                                 click -> buttonView.setChecked(!isChecked) ).show();
-            }else{
-                Snackbar.make(switch1, getResources().getString(R.string.snackbar_message_false), Snackbar.LENGTH_LONG).show();
-            }
-        });
+                }
+                else{
+                    Snackbar.make(switch1, "Switch is off",
+                            Snackbar.LENGTH_LONG).setAction("Undo",
+                            click -> buttonView.setChecked(!isChecked) ).show();
+                }
+                }
+
+    );
     }
 }
+
